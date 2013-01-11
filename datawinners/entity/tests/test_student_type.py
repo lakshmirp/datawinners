ad6a784212bec8b3cb55737c64185b92f5c3c006
lakshmirp <rp.lakshmi.45@gmail.com>
2013-01-11 17:21:58 +0530

    <Lakshmi/ashwin/pooja>Modified  setup functionalities

--- datawinners/entity/tests/test_student_type.py
+++ datawinners/entity/tests/test_student_type.py
@@ -4,12 +4,14 @@ from mangrove.bootstrap import initializer
 from mangrove.utils.test_utils.mangrove_test_case import MangroveTestCase
 
 class TestStudentType(MangroveTestCase):
-    def setUp(self):
-        MangroveTestCase.setUp(self)
-        initializer.run(self.manager)
+    ClassIsSetup = False
+    ClassIsTornDown = False
 
-    def tearDown(self):
-        MangroveTestCase.tearDown(self)
+    def setUp(self):
+        if not self.ClassIsSetup:
+            MangroveTestCase.setUp(self)
+            initializer.run(self.manager)
+            self.ClassIsSetup = True
 
     def test_should_create_student_type(self):
         create_student_type(self.manager)
@@ -17,11 +19,15 @@ class TestStudentType(MangroveTestCase):
         self.assertEquals('stu', subjects[0]['code'])
 
     def test_should_register_a_student_subject(self):
-        create_student_type(self.manager)
-        register_student_subject(self.manager,['DGPetrolBunk','bangalore','karnataka'], short_code="RP007",
+        register_student_subject(self.manager, ['DGPetrolBunk', 'bangalore', 'karnataka'], short_code="RP007",
             geometry={"type": "Point", "coordinates": [17.2833, 77.35]},
             lastname="RP", firstname="Lakshmi", description="This RP's petrol bunk!!.", mobile_number="9900099000")
         subjects = load_all_subjects(self.manager)
         short_code = subjects[0]['data'][0]['short_code']
-        self.assertEquals('RP007',short_code)
+        self.assertEquals('RP007', short_code)
+        self.ClassIsTornDown = True
+
+    def tearDown(self):
+        if self.ClassIsTornDown:
+            MangroveTestCase.tearDown(self)
 
